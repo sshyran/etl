6a6b624709d65f175a9b1833f149e52bb433648a
Gregory Russell <gfr@google.com>
2020-03-16 17:12:56 -0400

    hack out ndt7 fields

--- schema/ndt_result.go
+++ schema/ndt_result.go
@@ -6,16 +6,44 @@ import (
 	"cloud.google.com/go/bigquery"
 	"github.com/m-lab/annotation-service/api"
 	"github.com/m-lab/go/bqx"
-	"github.com/m-lab/ndt-server/data"
+	"github.com/m-lab/ndt-server/ndt5/c2s"
+	"github.com/m-lab/ndt-server/ndt5/control"
+	"github.com/m-lab/ndt-server/ndt5/s2c"
 )
+
+type NDTResult struct {
+	// GitShortCommit is the Git commit (short form) of the running server code.
+	GitShortCommit string
+	// Version is the symbolic version (if any) of the running server code.
+	Version string
+
+	// All data members should all be self-describing. In the event of confusion,
+	// rename them to add clarity rather than adding a comment.
+	ServerIP   string
+	ServerPort int
+	ClientIP   string
+	ClientPort int
+
+	StartTime time.Time
+	EndTime   time.Time
+
+	// ndt5
+	Control *control.ArchivalData `json:",omitempty"`
+	C2S     *c2s.ArchivalData     `json:",omitempty"`
+	S2C     *s2c.ArchivalData     `json:",omitempty"`
+
+	// ndt7
+	// Upload   *model.ArchivalData `json:",omitempty"`
+	// Download *model.ArchivalData `json:",omitempty"`
+}
 
 // NDTResultRow defines the BQ schema for the data.NDTResult produced by the
 // ndt-server for NDT client measurements.
 type NDTResultRow struct {
 	ParseInfo *ParseInfo
-	TestID    string         `json:"test_id,string" bigquery:"test_id"`
-	LogTime   int64          `json:"log_time,int64" bigquery:"log_time"`
-	Result    data.NDTResult `json:"result" bigquery:"result"`
+	TestID    string    `json:"test_id,string" bigquery:"test_id"`
+	LogTime   int64     `json:"log_time,int64" bigquery:"log_time"`
+	Result    NDTResult `json:"result" bigquery:"result"`
 }
 
 // Schema returns the BigQuery schema for NDTResultRow.
