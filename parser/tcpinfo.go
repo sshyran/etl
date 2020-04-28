adb102a282f886618aeaf0b3b38c260f9d43ce95
Gregory Russell <gfr@google.com>
2020-04-28 10:38:00 -0400

    restore full tcpinfo snaps

--- parser/tcpinfo.go
+++ parser/tcpinfo.go
@@ -169,7 +169,8 @@ func (p *TCPInfoParser) ParseAndInsert(fileMetadata map[string]bigquery.Value, t
 
 	row := schema.TCPRow{}
 	// TODO - restore full snapshots, or implement smarter filtering.
-	row.Snapshots = thinSnaps(snaps)
+	// row.Snapshots = thinSnaps(snaps)
+	row.Snapshots = snaps
 	row.FinalSnapshot = snaps[len(snaps)-1]
 	if row.FinalSnapshot.InetDiagMsg != nil {
 		row.SockID = row.FinalSnapshot.InetDiagMsg.ID.GetSockID()
